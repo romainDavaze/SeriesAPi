5998ae02b1b762aeaf36fa7ab7e704218fe43135
Romain Davaze <davaze.romain@gmail.com>
2017-04-25 10:21:39 +0200

    Adding swagger

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"encoding/json"
+	"strconv"
 )
 
 type Series struct {
@@ -19,14 +19,72 @@ type Series struct {
 
 type SoManySeries []Series
 
+var seriesObj SoManySeries
 
 func main() {
 
+	seriesObj = InitSeries()
+
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
+	//router.PathPrefix("/series")
+
+	router.HandleFunc("/series", SoManySeriesHandler).Methods("GET","POST")
+	router.HandleFunc("/series/{id}", SeriesHandler).Methods("GET","PUT","DELETE")
+
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+func SoManySeriesHandler(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+
+	if method == "GET" {
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(seriesObj)
+	} else if method == "POST" {
+		decoder := json.NewDecoder(r.Body)
+		var series Series
+		err := decoder.Decode(&series)
+		if err != nil {
+			log.Fatal(err)
+		}
+		series.Id = len(seriesObj)+1
+		seriesObj = append(seriesObj,series)
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
+func SeriesHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	method := r.Method
+	switch method {
+	case "GET":
+		series  := Series{}
+		for _, s := range seriesObj {
+			if strconv.Itoa(s.Id) == vars["id"] {
+				series = s
+			}
+		}
+
+		if (Series{}) == series {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(series)
+		}
+
+	case "PUT":
+	case "DELETE": w.WriteHeader(http.StatusNotImplemented)
+	}
+
+}
+
+func InitSeries() SoManySeries {
+ return SoManySeries{
+	 Series{Id: 1, Title: "Game Of Thrones", Type: "Heroic fantasy", Author: "Georges RR Martin", Seasons: 6},
+	 Series{Id: 2, Title: "Breaking Bad", Type: "Thriller", Author: "Vince Gilligan", Seasons: 5},
+	 Series{Id: 3, Title: "Daredevil", Type: "Superheroes", Author: "Drew Goddard", Seasons: 2},
+ }
 }
